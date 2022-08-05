177941f889ce02d0aa0f8ad5eafe5d0999bfe34b
Lindsay Evans <lindsay.evans@boomworks.com.au>
2022-08-05 11:54:41 +1000

    feat(analytics): switch to Application Insights basic version

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
-import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web-basic';
 
 const appInsights = new ApplicationInsights({
-  config: {
-    connectionString:
-      'InstrumentationKey=d999b489-4274-4a38-9526-0964fd808627;IngestionEndpoint=https://australiaeast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://australiaeast.livediagnostics.monitor.azure.com/',
-  },
+  instrumentationKey: 'd999b489-4274-4a38-9526-0964fd808627',
+  //   connectionString:
+  //     'InstrumentationKey=d999b489-4274-4a38-9526-0964fd808627;IngestionEndpoint=https://australiaeast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://australiaeast.livediagnostics.monitor.azure.com/',
 });
-appInsights.loadAppInsights();
-appInsights.trackPageView({
-  properties: {
+
+appInsights.track({
+  name: 'Microsoft.ApplicationInsights.AABA40BC-EB0D-44A7-96F5-ED2103E47AE9.Pageview',
+  data: {
     referrer: document.referrer,
     screenWidth: window.screen.width,
     screenHeight: window.screen.height,
   },
+  baseType: 'PageviewData',
+  baseData: {
+    name: document.title,
+    uri: location.href,
+  },
 });
+appInsights.flush();
