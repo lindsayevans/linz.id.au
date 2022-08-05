64610178d425e46472e7c3c3a9835733c43002a6
Lindsay Evans <lindsay.evans@boomworks.com.au>
2022-08-05 17:55:16 +1000

    refactor: TS cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { ApplicationInsights } from '@microsoft/applicationinsights-web-basic';
 
 const appInsights = new ApplicationInsights({
   instrumentationKey: 'd999b489-4274-4a38-9526-0964fd808627',
-  //   connectionString:
-  //     'InstrumentationKey=d999b489-4274-4a38-9526-0964fd808627;IngestionEndpoint=https://australiaeast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://australiaeast.livediagnostics.monitor.azure.com/',
 });
 
 appInsights.track({
@@ -19,4 +17,5 @@ appInsights.track({
     uri: location.href,
   },
 });
+
 appInsights.flush();
